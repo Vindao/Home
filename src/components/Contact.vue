bc5b28f645c9c5454924cfea3e1f1200031d03a9
VindaoCore <vindao@outlook.com>
2020-02-08 01:48:05 +0100

    changes

--- src/components/Contact.vue
+++ src/components/Contact.vue
@@ -1,15 +1,62 @@
 <template>
   <q-card class="contactModal">
-    <form class="contactForm">
-      <input type="text" />
-    </form>
+    <q-form class="contactForm">
+      <q-input
+        color="secondary"
+        dark
+        v-model="name"
+        type="text"
+        label="Name *"
+        lazy-rules
+        :rules="[val => (val && val.length > 0) || 'Please type something']"
+      />
+      <q-input
+        color="secondary"
+        dark
+        v-model="email"
+        type="email"
+        label="Email *"
+        lazy-rules
+        :rules="[val => (val && val.length > 0) || 'Please type something']"
+      />
+      <q-input
+        color="secondary"
+        dark
+        v-model="company"
+        label="Company"
+        lazy-rules
+        :rules="[val => (val && val.length > 0) || 'Please type something']"
+      />
+      <q-input
+        color="secondary"
+        dark
+        v-model="phone"
+        type="phone"
+        label="Phone"
+        :rules="[val => validate('phone', val) || 'Please type something']"
+      />
+    </q-form>
   </q-card>
 </template>
 
 <script lang="ts">
 import Vue from 'vue';
 export default Vue.extend({
-  name: 'Contact'
+  name: 'Contact',
+  data: () => ({
+    name: '',
+    email: '',
+    company: '',
+    phone: ''
+  }),
+  methods: {
+    validate(type: string, val: string) {
+      switch (type) {
+        case 'phone':
+          return val.match(/[+]?([0-9])([\s-]?)\w+/);
+      }
+    }
+  }
 });
 </script>
 
@@ -22,10 +69,15 @@ export default Vue.extend({
   height: calc(
     100vh - (2 * #{$rootMargin} + #{$headerHeight} + #{$bottomNavHeight})
   );
-  opacity: 0.75;
+  opacity: 0.95;
   background-color: $dark;
   .contactForm {
     width: 100%;
+    height: 100%;
+    display: flex;
+    flex-direction: column;
+    justify-content: center;
+    align-items: center;
   }
 }
 </style>
