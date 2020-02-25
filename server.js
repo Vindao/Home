abc4587fd54285d3aa90fde3f1c90a4bdd6954db
VindaoCore <vindao@outlook.com>
2020-02-25 01:37:25 +0100

    test

--- server.js
+++ server.js
@@ -1,16 +1,13 @@
-const express = require('express'),
-  serveStatic = require('serve-static'),
-  port = process.env.PORT || 5000;
+"use strict";
+const express = require('express'), serveStatic = require('serve-static'), port = process.env.PORT || 5000;
 const bodyParser = require('body-parser');
 const cors = require('cors');
 const app = express();
-
 // Middleware
 app.use(bodyParser.json());
 app.use(cors());
-
 const Test = require('./server/routes/test.js');
 app.use('/test', Test);
-
 app.use(serveStatic(__dirname + '/dist/pwa'));
 app.listen(port);
+//# sourceMappingURL=server.js.map
--- server/routes/test.ts
+++ server/routes/test.ts
@@ -1,9 +1,9 @@
-import express from 'express';
+const express = require('express');
 
 const Example = express.Router();
 
 Example.get('/example', (req, res, next) => {
-  res.send('This is an example route.');
+  res.send('This is an awesome example route.');
 });
 
 module.exports = Example;
