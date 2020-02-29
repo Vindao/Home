2bb578b59fdfc24fab49be0dd4d103adf66f701a
VindaoCore <vindao@outlook.com>
2020-02-29 04:54:14 +0100

    test

--- server/config/main.ts
+++ server/config/main.ts
@@ -1,7 +1,7 @@
 const DayMs = 1000 * 60 * 60 * 24;
 
 export const BaseClientUrl = 'https://vindao.herokuapp.com';
-export const BaseServerUrl = 'https://vindao_api.herokuapp.com';
+export const BaseServerUrl = 'https://vindaoapi.herokuapp.com';
 export const port = process.env.PORT || 5000;
 export const maxConfMailTokenAge = DayMs;
 export const sessMaxAge = 7 * DayMs;
