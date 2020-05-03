144a389f7e374f4055d9ca2bbcd0a06e933f7e13
Vindao <vindao@outlook.com>
2020-05-03 02:11:20 +0200

    initial gridsome

---
+++ src/admin/index.ts
@@ -0,0 +1,2 @@
+//@ts-ignore
+import CMS from 'netlify-cms';
---
+++ src/main.ts
@@ -0,0 +1,9 @@
+// This is the main.js file. Import global CSS and scripts here.
+// The Client API can be used here. Learn more: gridsome.org/docs/client-api
+
+import DefaultLayout from '~/layouts/Default.vue';
+
+export default (Vue: any, { router, head, isClient }: any) => {
+  // Set default layout as a global component
+  Vue.component('Layout', DefaultLayout);
+};
