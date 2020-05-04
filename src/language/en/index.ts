0dc7d46acbc2ca3d7ea830b055648654a19d2e1f
Vindao <vindao@outlook.com>
2020-05-04 12:53:36 +0200

    changes

--- src/language/en/index.ts
+++ src/language/en/index.ts
@@ -65,6 +65,79 @@ export default {
       By utilizing modern web technologies, PWA's feel and function like a native app, but are distributed via the web. 
       Therefore they are as accessible as any website, while still offering all the features and conveniences like native applications. 
       `
+    },
+    Cards: {
+      Hero: {
+        id: 1,
+        title: 'One application for all your devices',
+        text: `Progressive web applications are a new technology which brings native-like applications
+          to the web. It is a game changer because it enables us to create integrated applications
+          for all devices with one single codebase.`,
+        img:
+          'https://res.cloudinary.com/dhrg3xz0y/image/upload/f_auto/v1588126230/Vindao%20Home/responsive-2044932_zjwjbn.png'
+      },
+
+      Offline: {
+        id: 2,
+
+        title: 'Offline capable',
+        text: `Through intelligent caching techniques, our applications are fully offline capable. 
+        Therefore you will never have a slow experience because of a bad internet connection. 
+        After the initial load, a PWA will always load instantly.`,
+        img:
+          'https://res.cloudinary.com/dhrg3xz0y/image/upload/f_auto/v1588126230/Vindao%20Home/horizon-3332692_y1pec6'
+      },
+      Installable: {
+        id: 3,
+
+        title: 'Installable',
+        text: `Like any native application, a PWA can be installed to your system by just one click. 
+        After installation the application will be available like all other applications on your device. 
+        For instance, you can access the application via you homescreen on your smartphone, or the application launcher on your laptop or desktop PC.`,
+        img:
+          'https://res.cloudinary.com/dhrg3xz0y/image/upload/f_auto/v1588126230/Vindao%20Home/ehr-1476525_bfq1jt'
+      },
+      Consistent: {
+        id: 4,
+
+        title: 'Consistent User-experiences across all devices',
+        text: `Because progressive web applications are based on a single code-base, they are highly consistent across devices. 
+        Users can seemlessly switch between devices, and the application will still work and feel almost the same. 
+        Sure, there are differences between a mobile screen and the 4K monitor you use on your desktop PC, and the application has to visually change between those devices. 
+        But because PWA's are based on a single code-base the application will have a consistent feel across those devices.`,
+        img:
+          'https://res.cloudinary.com/dhrg3xz0y/image/upload/f_auto/v1588126230/Vindao%20Home/board-1273117_wnotff'
+      },
+      Discoverable: {
+        id: 5,
+
+        title: 'Discoverable',
+        text: `In contrast to native applictaions, which are only discoverable through the operating-systems intern app-store, PWA's are distributed via the web. 
+        This offers a lot of opportunities. Such as easily finding the application via major search engines, linking to the applictaion from any website ... . 
+        `,
+        img:
+          'https://res.cloudinary.com/dhrg3xz0y/image/upload/f_auto/v1588126230/Vindao%20Home/outlook-2208932_bpfalg'
+      },
+      Fire: {
+        id: 6,
+        title: 'Fast, Integrated, Reliable, and Engaging',
+        text: `Through background processing, and intelligent caching techniques, our applications are as fast as any native app installed on your phone or desktop.
+        Progressive web applications can be perfectly integrated with any hardware. If a feature is not available on a given device, the application is able to detect that and react accordingly. 
+        Reliability is a huge factor, especially in mobile applications, because a fast internet connection is not always available. Through the rich caching techniques, PWA's completely work offline. Therefore slow internet connections will not slow down the application. 
+        Because of the seamless integration of PWA's with the operating system, users stay much more engaged.`,
+        img:
+          'https://res.cloudinary.com/dhrg3xz0y/image/upload/f_auto/v1588126230/Vindao%20Home/freerider-498473_ktpzmh'
+      },
+      Interaction: {
+        id: 7,
+
+        title: 'Faster initial interaction',
+        text: `Because of their web-based nature, you can interact with an PWA much faster than with traditional native applications. 
+        It is as easy as opening a URL in your favorite browser. 
+        However, PWA's are also called hybrid apps, because they can be accessed through the browser, but if you want you can always install it natively.`,
+        img:
+          'https://res.cloudinary.com/dhrg3xz0y/image/upload/f_auto/v1588126230/Vindao%20Home/spaceman-4160023_zng5oa'
+      }
     }
   }
 };
--- src/pages/Services.vue
+++ src/pages/Services.vue
@@ -15,40 +15,116 @@
       </v-row>
       <v-row>
         <v-col class="col-12" v-for="edge in $static.allCard.edges" :key="edge.node.id">
-          <v-card v-if="edge.node.index % 2 && !$vuetify.breakpoint.xs">
-            <v-row align="center">
-              <v-col class="col-12 col-sm-7" fill-height>
-                <v-card-title style="top: 0;" class="justify-center">
-                  {{ edge.node[`${lang}_title`] }}
-                </v-card-title>
-                <v-card-text class="justify-center">
-                  {{ edge.node[`${lang}_text`] }}
-                </v-card-text>
-              </v-col>
-              <v-col class="col-12 col-sm-5 pt-0 pb-0">
-                <v-row align="center">
-                  <g-image style="width: 100%" :src="edge.node.image"> </g-image>
+          <!-- <v-card height="250" v-if="edge.node.index % 2 && !$vuetify.breakpoint.xs">
+            <v-container fill-height class="pt-0 pb-0">
+              <v-row align="center" style="height: 100%">
+                <v-col class="col-12 col-sm-7 ">
+                  <v-card-title style="top: 0;" class="justify-center">
+                    {{ edge.node[`${lang}_title`] }}
+                  </v-card-title>
+                  <v-card-text class="justify-center">
+                    {{ edge.node[`${lang}_text`] }}
+                  </v-card-text>
+                </v-col>
+                <v-col
+                  class="col-12 col-sm-5 pa-0"
+                  :style="{
+                    height: `100%`,
+                    backgroundImage: `url(${edge.node.image})`,
+                    backgroundSize: 'cover',
+                    backgroundPosition: 'center'
+                  }"
+                  align="center"
+                >
+                </v-col>
+              </v-row>
+            </v-container>
+          </v-card> -->
+
+          <v-container
+            v-if="!(edge.node.index % 2) && (!$vuetify.breakpoint.xs && !$vuetify.breakpoint.sm)"
+          >
+            <v-card>
+              <v-list-item class="px-0">
+                <v-row>
+                  <v-col class="col-12 col-md-7 text-center">
+                    <v-list-item-content>
+                      <v-card-title style="top: 0;" class="justify-center">
+                        {{ edge.node[`${lang}_title`] }}
+                      </v-card-title>
+                      <v-card-text class="justify-center">
+                        {{
+                          readMore.includes(edge.node.index)
+                            ? edge.node[`${lang}_text`]
+                            : edge.node[`${lang}_text`].slice(0, 200) + '...'
+                        }}
+                        <br />
+                      </v-card-text>
+                    </v-list-item-content>
+                    <Expand
+                      :style="{
+                        transform: `rotateZ(${
+                          readMore.includes(edge.node.index) ? '180deg' : '0deg'
+                        })`
+                      }"
+                      class="expand"
+                      @click="
+                        !readMore.includes(edge.node.index)
+                          ? readMore.push(edge.node.index)
+                          : (readMore = readMore.filter(x => x !== edge.node.index))
+                      "
+                    />
+                  </v-col>
+                  <v-col class="col-12 col-md-5 py-0">
+                    <v-list-item-content class="pa-0" style="height: 100%">
+                      <g-image style="width: 100%" :src="edge.node.image"></g-image>
+                    </v-list-item-content>
+                  </v-col>
                 </v-row>
-              </v-col>
-            </v-row>
-          </v-card>
-          <v-card v-else>
-            <v-row align="center">
-              <v-col class="col-12 col-sm-5 pt-0 pb-0 " align="center">
-                <v-row align="center">
-                  <g-image style="width: 100%" :src="edge.node.image"> </g-image>
+              </v-list-item>
+            </v-card>
+          </v-container>
+          <v-container v-else>
+            <v-card>
+              <v-list-item class="px-0">
+                <v-row>
+                  <v-col class="col-12 col-md-5 py-0">
+                    <v-list-item-content class="pa-0" style="height: 100%">
+                      <g-image style="width: 100%" :src="edge.node.image"></g-image>
+                    </v-list-item-content>
+                  </v-col>
+                  <v-col class="col-12 col-md-7 text-center">
+                    <v-list-item-content>
+                      <v-card-title style="top: 0;" class="justify-center">
+                        {{ edge.node[`${lang}_title`] }}
+                      </v-card-title>
+                      <v-card-text class="justify-center">
+                        {{
+                          readMore.includes(edge.node.index)
+                            ? edge.node[`${lang}_text`]
+                            : edge.node[`${lang}_text`].slice(0, 150) + '...'
+                        }}
+                        <br />
+                      </v-card-text>
+                    </v-list-item-content>
+                    <Expand
+                      :style="{
+                        transform: `rotateZ(${
+                          readMore.includes(edge.node.index) ? '180deg' : '0deg'
+                        })`
+                      }"
+                      class="expand"
+                      @click="
+                        !readMore.includes(edge.node.index)
+                          ? readMore.push(edge.node.index)
+                          : (readMore = readMore.filter(x => x !== edge.node.index))
+                      "
+                    />
+                  </v-col>
                 </v-row>
-              </v-col>
-              <v-col class="col-12 col-sm-7">
-                <v-card-title style="top: 0;" class="justify-center">
-                  {{ edge.node[`${lang}_title`] }}
-                </v-card-title>
-                <v-card-text class="justify-center">
-                  {{ edge.node[`${lang}_text`] }}
-                </v-card-text>
-              </v-col>
-            </v-row>
-          </v-card>
+              </v-list-item>
+            </v-card>
+          </v-container>
         </v-col>
       </v-row>
     </v-container>
@@ -57,7 +133,7 @@
 
 <static-query>
 query {
-  allCard(sortBy:"ID") {
+  allCard(sortBy:"index", order:ASC) {
     edges{
       node{
         index
@@ -78,8 +154,16 @@ query {
 import Vue from 'vue';
 import { mapGetters } from 'vuex';
 import { mapRoutes } from '../utils/router';
+//@ts-ignore
+import Expand from '@/assets/icons/expand.svg';
 
 export default Vue.extend({
+  components: {
+    Expand
+  },
+  data: () => ({
+    readMore: [] as string[]
+  }),
   computed: {
     ...mapGetters({ lang: 'Language/language', text: 'Language/text' })
   },
@@ -90,4 +174,8 @@ export default Vue.extend({
 });
 </script>
 
-<style></style>
+<style lang="scss" scoped>
+.expand {
+  cursor: pointer;
+}
+</style>
