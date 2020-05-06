1a0a80f83c43f631b540e32be3001f9a726ee630
Vindao <vindao@outlook.com>
2020-05-06 07:24:05 +0200

    changes

--- src/utils/initLanguage.ts
+++ src/utils/initLanguage.ts
@@ -3,21 +3,21 @@ import { LangCodeT } from '@/types/language';
 
 export default (): LangCodeT => {
   const langRaw = navigator.language.toLowerCase();
-  if (langRaw === 'de-de') {
-    console.log('german');
+  if (navigator) {
+    switch (langRaw) {
+      case 'nl':
+      case 'nl-be':
+        return 'nl';
+      case 'de':
+      case 'de-at':
+      case 'de-de':
+      case 'de-li':
+      case 'de-lu':
+      case 'de-ch':
+        return 'de';
+      default:
+        return 'en';
+    }
   }
-  switch (langRaw) {
-    case 'nl':
-    case 'nl-be':
-      return 'nl';
-    case 'de':
-    case 'de-at':
-    case 'de-de':
-    case 'de-li':
-    case 'de-lu':
-    case 'de-ch':
-      return 'de';
-    default:
-      return 'en';
-  }
+  return 'en';
 };
